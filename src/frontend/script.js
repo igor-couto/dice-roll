a6ca5234a4abba0edd327dfb53f2b9012bf9688d
Igor Couto <igor.fcouto@gmail.com>
2025-01-04 23:41:51 +0000

    Add frontend project

---
+++ src/frontend/script.js
@@ -0,0 +1,40 @@
+const audio = new Audio('./sounds/dice-throw.ogg');
+const diceElement = document.getElementById('dice');
+const buttonElement = document.querySelector("#roll-button > button");
+const buttonElementText = buttonElement.textContent;
+let diceRollResult = 1;
+
+audio.volume = 0.5;
+buttonElement.onclick = rollDice;
+
+async function rollDice() {
+    buttonElement.disabled = true;
+    buttonElement.textContent = 'Rolling...';
+
+    setTimeout(() => { 
+        buttonElement.textContent = buttonElementText;
+        buttonElement.disabled = false;
+    }, 2000);
+
+    audio.currentTime = 0;
+    audio.play();
+
+    try {
+        const response = await fetch('/roll');
+        if (!response.ok)
+            throw new Error('Network response was not ok');
+        
+        diceRollResult = await response.text();
+        console.log(diceRollResult);
+
+        for (let i = 1; i <= 6; i++) {
+            diceElement.classList.remove('show-' + i);
+        }
+        void diceElement.offsetWidth;
+        diceElement.classList.add('show-' + diceRollResult);
+    } catch (error) {
+        console.error('Error fetching dice roll result:', error);
+        buttonElement.textContent = buttonElementText;
+        buttonElement.disabled = false;
+    }
+}
